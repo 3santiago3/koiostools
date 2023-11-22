1f2f06443f8bac7e46b2a9a31384272c5819e477
runfeng li <lirf@smartheer.com>
2023-11-22 14:03:26 +0800

    fix: 添加注释

--- src/file.ts
+++ src/file.ts
@@ -1,7 +1,12 @@
+/**
+ * 判断给定的 Buffer 是否为 PDF 文件
+ * @param buf 要判断的 Buffer
+ * @returns 如果是 PDF 文件则返回 true，否则返回 false
+ */
 export const isPdf = (buf: Buffer): boolean => {
   return (
-    Buffer.isBuffer(buf) &&
-    buf.lastIndexOf('%PDF-') === 0 &&
-    buf.lastIndexOf('%%EOF') > -1
+    Buffer.isBuffer(buf) && // 判断是否为 Buffer
+    buf.lastIndexOf('%PDF-') === 0 && // 判断是否以 %PDF- 开头
+    buf.lastIndexOf('%%EOF') > -1 // 判断是否包含 %%EOF
   );
 };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './sum';
 export * from './type';
 export * from './string';
+export * from './file';
